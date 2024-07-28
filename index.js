b254ee74363dfad2360a29ea0e7f45755bd8ecdb
mysteriousCorgi <65299127+mysteriousCorgi@users.noreply.github.com>
2024-07-29 00:06:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuItems = document.querySelectorAll('.menu-item span');
+document.addEventListener("DOMContentLoaded", function() {
+    const menuItems = document.querySelectorAll(".menu-item > span");
 
     menuItems.forEach(function(item) {
-        item.addEventListener('click', function() {
+        item.addEventListener("click", function() {
             const submenu = this.nextElementSibling;
-            if (submenu.style.display === 'block') {
-                submenu.style.display = 'none';
-            } else {
-                submenu.style.display = 'block';
-            }
+            submenu.style.display = submenu.style.display === "none" || submenu.style.display === "" ? "block" : "none";
         });
     });
 });
