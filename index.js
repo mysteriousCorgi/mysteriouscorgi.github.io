8c52da6b71a4d7ec08944ed2fd19fd29d011dcea
mysteriousCorgi <65299127+mysteriousCorgi@users.noreply.github.com>
2024-11-26 23:35:44 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu(id) {
-    const menu = document.getElementById(id);
-    if (menu.style.display === "none" || menu.style.display === "") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-}
